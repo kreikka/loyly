0ae6a08f6e7091adc4093cb309075b4af0bb5eb7
Samuli Thomasson <simsaladin@paivola.fi>
2014-10-12 15:03:52 +0300

    extend member model, style active view in navbar

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -52,28 +52,34 @@ getPrivacyPolicyR =
         $(widgetFile "privacy-policy")
 
 memberForm :: Form Member
-memberForm = renderBootstrap2 $ Member
+memberForm = renderBootstrap2 $ Member False
     <$> areq textField "Koko nimi" Nothing
     <*> areq textField "Kotikunta" Nothing
     <*> areq emailField "Sähköposti" Nothing
     <*> areq checkBoxField "Olen HYY:n jäsen" Nothing
     <*> areq dayField "Syntymäaika" Nothing
     <*> areq (radioFieldList genderSelections) "Sukupuoli" Nothing
+    <*> areq textField "miten sait tietää meistä?" Nothing
+    <*> (fmap (fmap unTextarea)) (aopt textareaField "Löylyprofiilisi" Nothing)
   where 
       genderSelections :: [(Text, Maybe Bool)]
       genderSelections = [("Mies", Just True), ("Nainen", Just False), ("Muu", Nothing)]
 
 memberStatistics :: Widget
 memberStatistics = do
-    membs' <- handlerToWidget $ runDB $ selectList [] []
+    membs <- handlerToWidget $ runDB $ selectList [] []
 
-    let membs             = map entityVal membs'
-        total             = length membs
-        hyy               = length $ filter memberHyyMember membs
+    let membs'            = map entityVal membs
+        membsAppr         = filter memberApproved membs'
+        membsNotAppr      = filter (not . memberApproved) membs'
+        total             = length membsAppr
+        hyy               = length $ filter memberHyyMember membsAppr
         hyyP | total == 0 = 0
              | otherwise  = floor (100 * fromIntegral hyy / fromIntegral total :: Double) :: Int
 
     [whamlet|
 Jäseniä #{total}, joista HYYläisiä #{hyy} (#{hyyP}%).
+$if not $ null membsNotAppr
+    Lisäksi jäseneksi hyväksymistä odottaa #{length membsNotAppr}.
 |]
 
