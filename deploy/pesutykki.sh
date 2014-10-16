b05e74f23f24d51c89b4f71ff129f98fc46f6cb9
Tuomas Kinnunen <tuomas.kinnunen@paivola.fi>
2014-10-16 23:51:38 +0300

    fixed nav .active, updated deploy script

--- deploy/pesutykki.sh
+++ deploy/pesutykki.sh
@@ -3,27 +3,45 @@ set -o errexit -o xtrace -o nounset
 # WARNING: missing permission setup
 # assuming db and static/tmp permissions are already configured
 
-DESTINATION=/opt/saunahali/
+DESTINATION=/opt/saunahali
+USER=saunahali
 
 # scripts directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 
+
 # Build
 
 cd $DIR/../
-cabal clean
+
+if [[ $1 != "--fast" ]]
+    then cabal clean
+fi
 cabal build
 
 
-# Copy files and restart
 
-sudo mkdir -p $DESTINATION || true
+### Copy files and restart
 
-sudo cp -r $DIR/../{static,config} $DESTINATION
+SECRETS="$DESTINATION/config/client_session_key.aes $DESTINATION/config/settings.yml"
 
+# Directories
+sudo mkdir -p $DESTINATION/config || true
+
+# Files
+cd $DIR/../
+sudo cp -r static $DESTINATION
+sudo cp -r config/*[^~] $DESTINATION/config/
+sudo mv $DESTINATION/config/mysettings.yml $DESTINATION/config/settings.yml
+
+# Permissions
+sudo chmod og-rwx $SECRETS
+sudo chown $USER $DESTINATION $DESTINATION/static/tmp $SECRETS
+
+# Binary
 sudo systemctl stop loyly || true  # stop if running
-sudo cp $DIR/../dist/build/loyly/loyly $DESTINATION
+sudo cp dist/build/loyly/loyly $DESTINATION
 sudo systemctl start loyly
 
 
